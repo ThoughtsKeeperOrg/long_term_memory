dbb1b482fc0d2941c76fc805da141b07cadae64f
isidzukuri <axesigon@gmail.com>
2024-12-25 22:45:14 +0200

    publish kafka event when Thougt is updated

--- app/consumers/ocr_consumer.rb
+++ app/consumers/ocr_consumer.rb
@@ -10,8 +10,17 @@ class OcrConsumer < ApplicationConsumer
       # if message.payload['status'] == 'scanned'
 
       thought = Image.find(message.key).thought
-      thought.content += message.payload['text']
+      thought.content = [thought.content, message.payload['text']].join("\n")
       thought.save
+
+      Karafka.producer
+             .produce_sync(key: thought.id.to_s,
+                           topic: :entities_updates,
+                           payload: { entitity_type: :thought,
+                                      entity: thought,
+                                      status: :updated,
+                                      user_id: 'placeholder-user_id' }.to_json)
+
       # else
       #   # TODO
       # end
