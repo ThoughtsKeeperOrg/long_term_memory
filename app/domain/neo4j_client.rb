d542300920463b2569dfa4b262a83846330efd24
isidzukuri <axesigon@gmail.com>
2025-01-03 22:27:24 +0200

    fix typo in neo4j client setup

--- app/domain/neo4j_client.rb
+++ app/domain/neo4j_client.rb
@@ -4,7 +4,7 @@ class Neo4jClient
   def initialize
     url = URI::Generic.build(scheme: 'neo4j',
                              host: ENV.fetch('NEO4J_HOST', 'localhost'),
-                             port: ENV.fetch('NEO4J_HOST', '7687')).to_s
+                             port: ENV.fetch('NEO4J_PORT', '7687')).to_s
     auth_tokens = Neo4j::Driver::AuthTokens.basic(ENV.fetch('NEO4J_USER', 'neo4j'),
                                                   ENV.fetch('NEO4J_PASSWORD', 'your_password'))
     @driver = Neo4j::Driver::GraphDatabase.driver(url, auth_tokens)
