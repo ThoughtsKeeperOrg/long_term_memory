c61d40aff6701fc98aa75fb8ab0c195db286f273
isidzukuri <axesigon@gmail.com>
2025-01-03 11:44:58 +0200

    rubocop

--- app/consumers/ocr_consumer.rb
+++ app/consumers/ocr_consumer.rb
@@ -5,32 +5,31 @@ class OcrConsumer < ApplicationConsumer
   def consume
     messages.each do |message|
       # Rails.logger.debug message.payload
-      p message
-
       # if message.payload['status'] == 'scanned'
-
-      thought = Image.find(message.key).thought
-      thought.content = [thought.content, message.payload['text']].join("\n")
-      thought.save
-
-      Karafka.producer
-             .produce_sync(key: thought.id.to_s,
-                           topic: :entities_updates,
-                           payload: { entitity_type: :thought,
-                                      entity: thought,
-                                      status: :updated,
-                                      user_id: 'placeholder-user_id' }.to_json)
-
-      # else
-      #   # TODO
-      # end
-      # Thought.create(content: message.payload.to_json)
+      process_message(message)
+    # else
+    #   # TODO
+    # end
     rescue StandardError => e
       p '*' * 88
-      # p 'kafka message consuption error!!!'
+      p 'Message consuption error:'
       p e.message
       p e.backtrace.join("\n")
       p '*' * 88
     end
+  end
+
+  def process_message(message)
+    thought = Image.find(message.key).thought
+    thought.content = [thought.content, message.payload['text']].join("\n")
+    thought.save
+
+    Karafka.producer
+           .produce_sync(key: thought.id.to_s,
+                         topic: :entities_updates,
+                         payload: { entitity_type: :thought,
+                                    entity: thought,
+                                    status: :updated,
+                                    user_id: 'placeholder-user_id' }.to_json)
   end
 end
