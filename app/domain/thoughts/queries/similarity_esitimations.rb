cd5103fc02e73572eb2c6b5f01122e5218dda352
isidzukuri <axesigon@gmail.com>
2025-01-10 10:45:28 +0200

    extract cypher query to constant

--- app/domain/thoughts/queries/similarity_esitimations.rb
+++ app/domain/thoughts/queries/similarity_esitimations.rb
@@ -3,13 +3,15 @@
 module Thoughts
   module Queries
     class SimilarityEsitimations
+      CYPHER_QUERY = "MATCH (a:Thought { entity_id: $entity_id })-[similarity:similarity]-(node)
+                      WHERE similarity.estimation > 0.0
+                      RETURN node.entity_id as id, similarity.estimation as similarity
+                      ORDER BY similarity.estimation DESC"
+
       def call(entity_id)
         items = []
         db_client.session do |session|
-          query_result = session.run("MATCH (a:Thought { entity_id: $entity_id })-[similarity:similarity]-(node)
-                                WHERE similarity.estimation > 0.0
-                                RETURN node.entity_id as id, similarity.estimation as similarity
-                                ORDER BY similarity.estimation DESC", entity_id: entity_id.to_s)
+          query_result = session.run(CYPHER_QUERY, entity_id: entity_id.to_s)
 
           items = query_result.map(&:to_h)
         end
