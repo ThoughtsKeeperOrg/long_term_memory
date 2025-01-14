3ee8575dcb8323fd2eb266cb6eac5f05c43bffa1
isidzukuri <axesigon@gmail.com>
2025-01-14 14:57:40 +0200

    remove not supported actions

--- app/controllers/api/thoughts_controller.rb
+++ app/controllers/api/thoughts_controller.rb
@@ -18,23 +18,6 @@ module Api
       render json: result, status: result[:errors].present? ? 400 : 201
     end
 
-    def update
-      @thought = Thought.find(params[:id])
-      @thought.attributes = thought_params
-      @thought.save
-      if @thought.errors.any?
-        render json: @thought.errors.full_messages, status: :bad_request
-      else
-        head :ok
-      end
-    end
-
-    def destroy
-      Thought.find(params[:id]).destroy
-
-      head :ok
-    end
-
     def thought_params
       params.permit(thought: [:content],
                     file: %i[filename type convert_to_text file_base64])
